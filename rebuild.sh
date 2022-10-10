6efabc899f91e5df2aba4a98421f417b5d790418
menguozi <menguozi@outlook.com>
2022-10-10 07:38:01 +0000

    Do not hack the source code.

--- rebuild.sh
+++
@@ -1,9 +0,0 @@
-make M=fs/f2fs -j64
-sudo umount /mnt/f2fs
-sudo rmmod f2fs
-sudo insmod /home/menguozi/workspace/hisilicon/fs/f2fs/f2fs.ko
-sudo mkfs.f2fs -f /dev/sdc
-sudo mount /dev/sdc /mnt/f2fs
-sudo cat /sys/kernel/debug/f2fs/status >> start
-
-sudo cat /sys/kernel/debug/f2fs/status >> end
